c1502837198db58c8ab414484078c942e76cf439
Christian Cordeiro <christiancordeiro96@gmail.com>
2024-02-26 13:23:35 -0300

    Adiciona sistema de conexão do front-end com o backend Sanity + página de login

---
+++ shareme_frontend/src/client.jsx
@@ -0,0 +1,17 @@
+import { createClient } from "@sanity/client"
+import imageUrlBuilder from "@sanity/image-url"
+
+//createClient é uma instancia do sanity/client, está sendo responsável por interagir com o backend e sendo passada pro componente de Login
+export const client = createClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2021-10-21",
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
+})
+
+// instanciando o construtor de imagem (associado ao sanity/image-url) baseado na config da variavel client
+const builder = imageUrlBuilder(client)
+
+// utilizando função "builder" do imageUrlBuilder para gerar uma URL
+export const urlFor = (source) => builder.image(source)
